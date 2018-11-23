d2b5d2d16bb5fef516e4e33521cb8007008a91fa
Billy Angermeier <william.r.angermeier@gmail.com>
2018-11-23 18:49:23 -0500

    css changes, hamburger menu

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {logout} from '../actions'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../actions'
 import './sidebar.css';
 
 export function SideBar(props) {
@@ -9,25 +9,31 @@ export function SideBar(props) {
     let login;
     let log;
     let userId = localStorage.id ? localStorage.id : null
-    if(userId) {
+    if (userId) {
         userId = <a href={`/profile/${userId}`}>Profile</a>
-        log=<a href="/signin">Logout</a>
+        log = <a href="/signin">Logout</a>
     } else {
         log = <a href="/signup">Sign Up</a>
-        login= <a href="/signin">Log In</a>
+        login = <a href="/signin">Log In</a>
     }
-    
+
     return (
-    
+
         <div className="dropdown">
-  <button className="dropbtn">Menu</button>
-  <div className="dropdown-content">
-    <a href="/home">Home</a>
-    {userId}
-    {log}
-    {login}
-  </div>
-</div>
+            <div className="dropbtn">
+                <div className="hamburger-box">
+                    <div className="hamburger-inner">
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="dropdown-content">
+                <a href="/home">Home</a>
+                {userId}
+                {log}
+                {login}
+            </div>
+        </div>
     )
 }
 
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import './welcome.css';
+import {Link} from 'react-router-dom'
 
 
 class Welcome extends React.Component {
 
     render() {
         return (
-            <div className="welcome-screen">
-                <div className="intro">
-                    <h2>Welcome!!!</h2>
-                    <p>View and post songs that sound similar and get stuck in your head
-            </p>
-                </div>
+            <div>
+                <div className="welcome-screen">
+                    <div className="intro">
+                        <p>Share songs that sound similar and get stuck in your head
+                     </p>
+                        <p>To see the newest EarBugs</p>
+                        <Link className="button" to="/home">
+                            Click Here
+                        </Link>
+                    </div>
 
+
+                </div>
                 <div className="links">
-                    <p>To view some of our content click <a href="/home">Here</a></p>
-                    <p>To sign up and create some of your own click <a href="/signup">Here</a></p>
-                    <p>Already have an account? <a href="/signin">Log In</a> </p>
+                    <div className="signup">
+                        <p>To start making EarBugs</p>
+                        <Link className="button" to="/signup">Click Here</Link>
+
+                    </div>
+                    <div className="account">
+                        <p>Already have an account? </p>
+                            <Link className="button" to="/signin">Log In</Link> 
+                    </div>
                 </div>
-
-
                 <div className="builtWith">
                     <p>This app was build using React, Redux, YouTube React, Mongo,
                          Mongoose, Express, Mocha, Chai, Jest
